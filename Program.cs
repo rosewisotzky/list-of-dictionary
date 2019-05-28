2fb0c4c844c8b1e308083265c7ad9819dab07929
Rose Wisotzky <rose@rosewisotzky.com>
2019-05-28 14:40:11 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace list_of_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+## list of dictionary and words
